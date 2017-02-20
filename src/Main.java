583d9f79777bf7a8967aaca33a7e4003be90d665
Ivan <ijaimes2872@gmail.com>
2017-02-19 19:22:06 -0600

    added main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import it.sauronsoftware.jave.*;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        mp3Convert converter = new mp3Convert();
+    }
+
+}
